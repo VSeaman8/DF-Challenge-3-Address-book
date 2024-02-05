14abb3feb96094390d41bb12fc2f314c0c585b47
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-02-05 16:38:48 +0000

    NEW: Test 2 Checked that added contact information is correctly stored in the address book - Passed

--- src/test/java/com/app/ContactManagerTests.java
+++ src/test/java/com/app/ContactManagerTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -27,6 +28,23 @@ public class ContactManagerTests {
         }
 
         }
+        @Nested
+    @DisplayName("Test 2 Added contact information is correctly stored in the address book")
 
-    }
+    class ContactInfoCorrectlyStoredInAddressBook {
+        @Test
+        void ContactInfoInAddressBook(){
+            // Arrange
+            ContactManager contactManager = new ContactManager();
+            Contact contact = new Contact("Harry Potter", "harrypotter@hogwarts.co.uk", "1234567890");
+            // Act
+            contactManager.addContact(contact);
+            // Assert
+            assertEquals(1, contactManager.getContacts().size(), "Contact should be added to the address book");
+            assertEquals(contact, contactManager.getContacts().get(0), "Contact details should match the added contact");
+        }
+        }
+        }
+
+
 
