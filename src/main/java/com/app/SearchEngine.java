e8fd93c2bb37ac3db6b14054dc8d5e6d2e3ffb3f
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-02-06 11:37:44 +0000

    UPDATED: Test 5 Passed

--- src/main/java/com/app/SearchEngine.java
+++ src/main/java/com/app/SearchEngine.java
@@ -1,5 +1,19 @@
 package com.app;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchEngine {
+
+    public List<Contact> searchName(String name, List<Contact> contacts){
+        List<Contact> searchResults = new ArrayList<>();
+
+        for (Contact contact : contacts){
+            if (contact.getName().equalsIgnoreCase(name)){
+                searchResults.add(contact);
+            }
+        }
+        return searchResults;
+    }
 }
